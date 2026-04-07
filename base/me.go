991b5f71545a886dad54ab3dbd0d408a0a08e95a
ochuluemmanuella-blip <ochuluemmnauella@gmail.com>
2026-04-07 13:55:16 +0100

    merlin

--- base/me.go
+++ base/me.go
@@ -8,12 +8,12 @@ import (
 func lastN(text []string) []string {
 	for i := 0; i < len(text); i++ {
 		if text[i] == "(low)" {
-			text[i] = strings.ToUpper(text[i])
+			text[i-1] = strings.ToUpper(text[i-1])
 		}
 		text = append(text[:i], text[i+1:]...)
 	}
 	return text
 }
 func main() {
-	fmt.Println(lastN([]string{"go is fun (up)"}))
+	fmt.Println(lastN([]string{"go", "is", "fun", "(up)"}))
 }
