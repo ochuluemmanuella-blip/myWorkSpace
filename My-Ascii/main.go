48b5278642d1aeb0b29eef1cc9680e75669fbc1e
aochulu <ochuluemmanuella@gmail.com>
2026-05-07 03:35:02 +0100

    understanding

---
+++ My-Ascii/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("wrong format of arguments! Usage: go run . [STRING]")
+		os.Exit(1)
+	}
+	input := os.Args[1]
+
+	content := loadBanner("standard.txt")
+
+	// fmt.Println(content[:50])
+	banner := parseBanner(content)
+	// fmt.Println(banner[23])
+	// fmt.Println(len(banner))
+	bannerMap := buildMap(banner)
+	// fmt.Println(bannerMap['H'])
+	// fmt.Println(bannerMap['!'])
+	validateInput(input, bannerMap)
+
+}
+func loadBanner(filename string) string {
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("error reading file: %v\n", err)
+		os.Exit(1)
+	}
+	content := strings.ReplaceAll(string(data), "\r\n", "\n")
+	return content
+}
+func parseBanner(content string) [][]string {
+	lines := strings.Split(content, "\n\n")
+	con := [][]string{}
+	for _, r := range lines {
+		res := strings.Split(r, "\n")
+		con = append(con, res)
+
+	}
+
+	return con
+}
+func buildMap(banner [][]string) map[rune][]string {
+	store := make(map[rune][]string)
+	for i := 0; i < 95; i++ {
+		char := rune(' ') + rune(i)
+		store[char] = banner[i]
+	}
+	return store
+}
+func validateInput(input string, bannerMap map[rune][]string) {
+	for i, char := range input {
+		if char == '\\' && i+1 < len(input) && input[i+1] == 'n' {
+			continue
+		}
+		if char == 'n' && i > 0 && input[i-1] == '\\' {
+			continue
+		}
+		_, ok := bannerMap[char]
+		if !ok {
+			fmt.Printf("Invalid character: %c\n", char)
+			os.Exit(1)
+		}
+	}
+}
